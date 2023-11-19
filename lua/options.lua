fac1c1bc58212d2508aba4d985e0e00cf0ecb696
Bartek Bubacz <bartosz.bubacz@gmail.com>
2023-11-19 22:40:04 +0100

    Updates

---
+++ lua/options.lua
@@ -0,0 +1,17 @@
+vim.o.hlsearch = false
+vim.wo.number = true
+vim.o.mouse = 'a'
+vim.o.clipboard = 'unnamed,unnamedplus'
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.wo.signcolumn = 'yes'
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.completeopt = 'menuone,noselect'
+vim.o.termguicolors = true
+vim.o.list = true
+vim.opt.listchars = { extends = '.', trail = '.', precedes = '|', nbsp = '_', eol = '↲' }
+
+-- vim: ts=2 sts=2 sw=2 et
