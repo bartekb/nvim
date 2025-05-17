4a3640b675b6ad9418daa9c5889ce1415f47a05f
Bartek Bubacz <bartosz.bubacz@gmail.com>
2025-05-17 15:51:07 +0200

    Added vim global to ingnored

--- lua/plugins/configs/lsp.lua
+++ lua/plugins/configs/lsp.lua
@@ -1,6 +1,22 @@
 local lspconfig = require "lspconfig"
 
-lspconfig.lua_ls.setup {}
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
 lspconfig.gopls.setup {}
 -- lspconfig.pyright.setup({})
 lspconfig.ts_ls.setup {}
