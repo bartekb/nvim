3bfb29fc4a3f2274aa75a0390159493389f20e56
Bartek Bubacz <bartosz.bubacz@gmail.com>
2025-05-17 12:38:47 +0200

    Added more TS lngs

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,9 +1,24 @@
 require("nvim-treesitter.configs").setup {
-    ensure_installed = { "lua", "vim", "vimdoc", "html", "css", "typescript", "javascript" },
+  ensure_installed = {
+    "lua",
+    "vim",
+    "vimdoc",
+    "html",
+    "css",
+    "typescript",
+    "javascript",
+    "ruby",
+    "yaml",
+    "go",
+    "rust",
+    "python",
+    "bash",
+    "sql",
+  },
 
-    highlight = {
-        enable = true,
-        use_languagetree = true,
-    },
-    indent = { enable = true },
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+  indent = { enable = true },
 }
